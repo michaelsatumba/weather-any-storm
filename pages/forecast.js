ffa776c4b7a64699b20b6feb60a7272cd95ac076
michaelsatumba <mjsatumba@gmail.com>
2022-04-06 10:40:31 -0700

    import React removed

--- pages/forecast.js
+++ pages/forecast.js
@@ -1,7 +1,5 @@
 import Link from 'next/link';
-import React from 'react';
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 function forecast() {
 	const [city, setCity] = useState(null);
