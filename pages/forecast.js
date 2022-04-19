0ab3e1bb20b5a8bf1bad9dbcba6ae0d3d32ea1c7
michaelsatumba <mjsatumba@gmail.com>
2022-04-19 10:49:49 -0700

    function Forecast

--- pages/forecast.js
+++ pages/forecast.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
-function forecast() {
+function Forecast() {
 	const [city, setCity] = useState(null);
 	const [weatherIcon, setWeatherIcon] = useState('10d');
 	const [weatherIconTomorrow, setWeatherIconTomorrow] = useState('10d');
@@ -260,4 +260,4 @@ function forecast() {
 	);
 }
 
-export default forecast;
+export default Forecast;
